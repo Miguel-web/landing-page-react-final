0b71bc8a70e1fd3a6f67d89fa2c4bbddaa048d45
Miguel Gil F <67928892+Miguel-web@users.noreply.github.com>
2023-06-27 02:45:01 +0000

    cleanup errors

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,71 +2,71 @@ import React from "react";
 
 const Card = () => {
   return (
-    <div class="card-group">
-      <div class="card text-center">
+    <div className="card-group">
+      <div className="card text-center">
         <img
-          src="https://api.lorem.space/image/album?w=150&h=150"
-          class="card-img-top"
+          src="https://picsum.photos/200/300?grayscale"
+          className="card-img-top"
         ></img>
-        <div class="card-body">
-          <h5 class="card-title">title</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">title</h5>
+          <p className="card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam odit atque adipisci quidem non debitis. Quo voluptatibus, numquam iusto repellat, dolorum, adipisci dicta molestiae in perferendis consequuntur totam perspiciatis aliquid?
           </p>
         </div>
-        <div class="card-footer text-center">
-          <a href="#" class="btn btn-primary">
+        <div className="card-footer text-center">
+          <a href="#" className="btn btn-primary">
             Go somewhere
           </a>{" "}
         </div>
       </div>
-      <div class="card text-center">
+      <div className="card text-center">
         <img
-          src="https://api.lorem.space/image/album?w=150&h=150"
-          class="card-img-top"
+          src="https://picsum.photos/200/300/?blur"
+          className="card-img-top"
         ></img>
-        <div class="card-body">
-          <h5 class="card-title">title</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">title</h5>
+          <p className="card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam minima voluptatum impedit consequatur laboriosam ullam praesentium. Nam eveniet, atque voluptatum ab ad, debitis repellendus distinctio, excepturi facilis culpa incidunt.
           </p>
         </div>
-        <div class="card-footer text-center">
-          <a href="#" class="btn btn-primary">
+        <div className="card-footer text-center">
+          <a href="#" className="btn btn-primary">
             Go somewhere
           </a>{" "}
         </div>
       </div>
-      <div class="card text-center">
+      <div className="card text-center">
         <img
-          src="https://api.lorem.space/image/album?w=150&h=150"
-          class="card-img-top"
+          src="https://picsum.photos/id/237/200/300"
+          className="card-img-top"
         ></img>
-        <div class="card-body">
-          <h5 class="card-title">title</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">title</h5>
+          <p className="card-text">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius illum laborum ipsa nobis iure nam molestias, reiciendis unde at distinctio quis impedit et voluptatibus eos tenetur dolorum iusto. Minima, est!
           </p>
         </div>
-        <div class="card-footer text-center">
-          <a href="#" class="btn btn-primary">
+        <div className="card-footer text-center">
+          <a href="#" className="btn btn-primary">
             Go somewhere
           </a>{" "}
         </div>
       </div>
-      <div class="card text-center">
+      <div className="card text-center">
         <img
-          src="https://api.lorem.space/image/album?w=150&h=150"
-          class="card-img-top"
+          src="https://picsum.photos/seed/picsum/200/300"
+          className="card-img-top"
         ></img>
-        <div class="card-body">
-          <h5 class="card-title">title</h5>
-          <p class="card-text">
+        <div className="card-body">
+          <h5 className="card-title">title</h5>
+          <p className="card-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque debitis eius unde aut consectetur eligendi minima optio tenetur pariatur voluptate? Qui possimus vero delectus quam. Fugiat maiores dicta optio sequi.
           </p>
         </div>
-        <div class="card-footer text-center">
-          <a href="#" class="btn btn-primary">
+        <div className="card-footer text-center">
+          <a href="#" className="btn btn-primary">
             Go somewhere
           </a>{" "}
         </div>
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <body className="d-flex flex-column">
   <footer className="flex-shrink-0 py-4 bg-dark text-white-50">
     <div className="container text-center">
       <small>Copyright © 2023 HubSpot, Inc.</small>
     </div>
   </footer>
-</body>
+
   );
 };
 export default Footer;
